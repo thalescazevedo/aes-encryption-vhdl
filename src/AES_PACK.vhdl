183ade6f4e76669f0245d60c801d178d3c83bccd
Thales Azevedo <thales4zev@gmail.com>
2026-06-15 15:05:57 -0300

    Sintaxe AES PACK
    Sintaxe AES PACK

--- src/AES_PACK.vhdl
+++ src/AES_PACK.vhdl
@@ -29,9 +29,9 @@ package AES_pack is
     function vetor128bits_to_matriz_4x4(input : std_logic_vector(127 downto 0)) return matriz_4x4; -- funcao que converte o vetor para matriz,
         -- com a ressalva de que o preenchimento e feito por colunas
     
-    function xtime(b : std_logic_vector(7 downto 0)) return std_logic_vector(7 downto 0); -- funcao de multiplicar por 2 e evitar overflow com xor (nicolas usa essa tambem)
+    function xtime(b : std_logic_vector(7 downto 0)) return std_logic_vector; -- funcao de multiplicar por 2 e evitar overflow com xor (nicolas usa essa tambem)
     
-    function matriz_4x4_to_128bits(input: matriz_4x4) return std_logic_vector(127 downto 0);
+    function matriz_4x4_to_128bits(input: matriz_4x4) return std_logic_vector;
 
 end package AES_pack;
 
@@ -182,7 +182,7 @@ package body AES_pack is
         return output;
     end function;
 
-    function xtime(b : std_logic_vector(7 downto 0)) return std_logic_vector(7 downto 0) is
+    function xtime(b : std_logic_vector(7 downto 0)) return std_logic_vector is
         variable result : std_logic_vector(7 downto 0);
     begin
         result := b(6 downto 0) & '0';
@@ -192,7 +192,7 @@ package body AES_pack is
         return result;
     end function;
 
-    function matriz_4x4_to_128bits(input: matriz_4x4) return std_logic_vector(127 downto 0) is
+    function matriz_4x4_to_128bits(input: matriz_4x4) return std_logic_vector is
         variable output : std_logic_vector(127 downto 0);
     begin
         for i in 0 to 3 loop
